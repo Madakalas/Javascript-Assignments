af45294dc3465884417e905ee9ec043e2d6e3d68
SIDDU REDDY <116141072+Madakalas@users.noreply.github.com>
2023-12-12 15:47:32 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,17 +1,103 @@
-function Solve(val) {
-    var v = document.getElementById('res');
-    v.value += val;
- }
- function Result() {
-    var num1 = document.getElementById('res').value;
-    var num2 = eval(num1);
-    document.getElementById('res').value = num2;
- }
- function Clear() {
-    var inp = document.getElementById('res');
-    inp.value = '';
- }
- function Back() {
-    var ev = document.getElementById('res');
-    ev.value = ev.value.slice(0,-1);
- }
+const expenseForm = 
+    document.getElementById("expense-form"); 
+const expenseList = 
+    document.getElementById("expense-list"); 
+const totalAmountElement = 
+    document.getElementById("total-amount"); 
+  
+// Initialize expenses array from localStorage 
+let expenses =  
+    JSON.parse(localStorage.getItem("expenses")) || []; 
+  
+// Function to render expenses in tabular form 
+function renderExpenses() { 
+  
+    // Clear expense list 
+    expenseList.innerHTML = ""; 
+  
+    // Initialize total amount 
+    let totalAmount = 0; 
+  
+    // Loop through expenses array and create table rows 
+    for (let i = 0; i < expenses.length; i++) { 
+        const expense = expenses[i]; 
+        const expenseRow = document.createElement("tr"); 
+        expenseRow.innerHTML = ` 
+      <td>${expense.name}</td> 
+      <td>$${expense.amount}</td> 
+      <td class="delete-btn" data-id="${i}">Delete</td> 
+    `; 
+        expenseList.appendChild(expenseRow); 
+  
+        // Update total amount 
+        totalAmount += expense.amount; 
+    } 
+  
+    // Update total amount display 
+    totalAmountElement.textContent = 
+        totalAmount.toFixed(2); 
+  
+    // Save expenses to localStorage 
+    localStorage.setItem("expenses",  
+        JSON.stringify(expenses)); 
+} 
+  
+// Function to add expense 
+function addExpense(event) { 
+    event.preventDefault(); 
+  
+    // Get expense name and amount from form 
+    const expenseNameInput = 
+        document.getElementById("expense-name"); 
+    const expenseAmountInput = 
+        document.getElementById("expense-amount"); 
+    const expenseName = 
+        expenseNameInput.value; 
+    const expenseAmount = 
+        parseFloat(expenseAmountInput.value); 
+  
+    // Clear form inputs 
+    expenseNameInput.value = ""; 
+    expenseAmountInput.value = ""; 
+  
+    // Validate inputs 
+    if (expenseName === "" || isNaN(expenseAmount)) { 
+        alert("Please enter valid expense details."); 
+        return; 
+    } 
+  
+    // Create new expense object 
+    const expense = { 
+        name: expenseName, 
+        amount: expenseAmount, 
+    }; 
+  
+    // Add expense to expenses array 
+    expenses.push(expense); 
+  
+    // Render expenses 
+    renderExpenses(); 
+} 
+  
+// Function to delete expense 
+function deleteExpense(event) { 
+    if (event.target.classList.contains("delete-btn")) { 
+  
+        // Get expense index from data-id attribute 
+        const expenseIndex = 
+            parseInt(event.target.getAttribute("data-id")); 
+  
+        // Remove expense from expenses array 
+        expenses.splice(expenseIndex, 1); 
+  
+        // Render expenses 
+        renderExpenses(); 
+    } 
+} 
+  
+// Add event listeners 
+expenseForm.addEventListener("submit", addExpense); 
+expenseList.addEventListener("click", deleteExpense); 
+  
+// Render initial expenses on page load 
+renderExpenses();
